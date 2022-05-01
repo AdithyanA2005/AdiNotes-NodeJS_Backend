7c78edb8a2588c50cdbf121327db28cdd79a51f7
Adithyan A <deepaadithyan56@gmail.com>
2022-05-01 17:13:32 +0530

    Basic create new user feature added

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  obj = {
-    name: "thanos",
-    age: "5 months",
-  };
-  res.json(obj);
+// Create A New User
+router.post("/", (req, res) => {
+  const user = User(req.body);
+  user.save();
+  res.send(req.body);
+  console.log(req.body);
 });
 
 module.exports = router;
