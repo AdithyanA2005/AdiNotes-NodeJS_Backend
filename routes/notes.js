5ef9eaa8ac7f78e8a5b69ee8e59fc856411d6295
Adithyan A <deepaadithyan56@gmail.com>
2022-05-03 09:24:21 +0530

    Route To Update a note added

--- routes/notes.js
+++ routes/notes.js
@@ -34,4 +34,54 @@ router.get("/getnotes", fetchuser, async (req, res) => {
   }
 });
 
+// UPDATE NOTE OF A USER | /api/v1/notes/updatenote/:id | auth required | PUT
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  try {
+    // Create a updatedValues onject with values which need to be updated
+    const updatedValues = {};
+    const { title, description, tag } = req.body;
+    if (tag) updatedValues.tag = tag;
+    if (title) updatedValues.title = title;
+    if (description) updatedValues.description = description;
+
+    // If note to update doesn't exists
+    let note = await Note.findById(req.params.id);
+    if (!note) return res.status(404).send("Not Found");
+    // UPDATE NOTE OF A USER | /api/v1/notes/updatenote/:id | auth required | PUT
+    router.put("/updatenote/:id", fetchuser, async (req, res) => {
+      try {
+        // Create a updatedValues onject with values which need to be updated
+        const updatedValues = {};
+        const { title, description, tag } = req.body;
+        if (tag) updatedValues.tag = tag;
+        if (title) updatedValues.title = title;
+        if (description) updatedValues.description = description;
+
+        // If note to update doesn't exists
+        let note = await Note.findById(req.params.id);
+        if (!note) return res.status(404).send("Not Found");
+
+        // If the note to be updated is not owned by the user return error
+        if (note.user.toString() !== req.user.id) return res.status(401).send("Not allowed");
+
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: updatedValues }, { new: true });
+        res.json(note);
+      } catch (error) {
+        // TODO: Add Logger
+        console.error(error);
+        res.status(500).send("Some Internal Error Occured in API: ");
+      }
+    });
+    // If the note to be updated is not owned by the user return error
+    if (note.user.toString() !== req.user.id) return res.status(401).send("Not allowed");
+
+    note = await Note.findByIdAndUpdate(req.params.id, { $set: updatedValues }, { new: true });
+    res.json(note);
+  } catch (error) {
+    // TODO: Add Logger
+    console.error(error);
+    res.status(500).send("Some Internal Error Occured in API: ");
+  }
+});
+
 module.exports = router;
