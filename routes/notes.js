39fbdd867a536a5f5fb84db9cb3fd1734e3213bd
Adithyan A <deepaadithyan56@gmail.com>
2022-05-03 09:14:17 +0530

    small comments added

--- routes/notes.js
+++ routes/notes.js
@@ -9,8 +9,9 @@ const router = express.Router();
 
 // CREATE NEW NOTE FOR A USER | /api/v1/notes/addnote | auth required | POST
 router.post("/addnote", addNoteChecksArray, getValidationError, fetchuser, async (req, res) => {
-  const { title, description, tag } = req.body;
   try {
+    // Get the datas from the req and create a new note in the logged in user account and send the note
+    const { title, description, tag } = req.body;
     const note = await Note.create({ title, description, tag, user: req.user.id });
     res.json(note);
   } catch (error) {
@@ -23,6 +24,7 @@ router.post("/addnote", addNoteChecksArray, getValidationError, fetchuser, async
 // FETCH ALL NOTES OF A USER | /api/v1/notes/getnotes | auth required | GET
 router.get("/getnotes", fetchuser, async (req, res) => {
   try {
+    // Fetch notes of a logged in user with his id and return the notes
     const notes = await Note.find({ user: req.user.id });
     res.json(notes);
   } catch (error) {
